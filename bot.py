6e65b48c0e2c4bc69a0aacba3f67c2e7cd3842d6
Bilal-Ehsan <bilalehsan933@gmail.com>
2021-12-20 15:18:13 +0000

    Chatbot now works with a superhero API

--- bot.py
+++ bot.py
@@ -4,9 +4,11 @@ import requests
 import aiml
 from termcolor import colored
 import pyttsx3
+import webbrowser
 
 
-api_key = '0e38947d9fd0fa5969c4735f293ab28c'
+weather_api_key = '0e38947d9fd0fa5969c4735f293ab28c'
+superhero_api_key = '3106208826265119'
 engine = pyttsx3.init() # Engine instance for the speech synthesis
 
 # The Kernel object is the public interface to the AIML interpreter
@@ -14,15 +16,43 @@ kern = aiml.Kernel()
 kern.setTextEncoding(None)
 kern.bootstrap(learnFiles='bot.xml')
 
-print(colored('\033[1mWelcome to the chatbot! I\'m good at talking about superheroes (or villains!)\033[0m', 'green'))
+print(colored('\033[1mWelcome to the chatbot! I like to talk about superheroes!\033[0m', 'green'))
 print(colored('\033[1mFor a cool list of prompts, enter \x1B[3mprompts\x1B[0m!\033[0m', 'green'))
 
 
 def show_prompts():
     print(colored('\nTry asking...\n', 'cyan'))
-    print('Placeholder')
-    print('Placeholder')
-    print('Placeholder\n')
+    print('Show me the stats of \033[1m[superhero]\033[0m')
+    print('Show me a picture of \033[1m[superhero]\033[0m')
+    print('Placeholder...\n')
+
+
+def show_stats(superhero):
+    response = requests.get(f'https://superheroapi.com/api/{superhero_api_key}/search/{superhero}')
+    if response.status_code == 200:
+        response_json = json.loads(response.content)
+        print(response_json)
+        power_stats = response_json['results'][0]['powerstats']
+        biography = response_json['results'][0]['appearance']
+        work = response_json['results'][0]['work']
+        print(f'-> {superhero} related info:\n')
+
+        for x, y in power_stats.items():
+            print(f'{x.capitalize()} - {y}')
+        print('')
+        for x, y in biography.items():
+            print(f'{x.capitalize()} - {y}')
+        print('')
+        for x, y in work.items():
+            print(f'{x.capitalize()} - {y}')
+
+
+def show_image(superhero):
+    response = requests.get(f'https://superheroapi.com/api/{superhero_api_key}/search/{superhero}')
+    if response.status_code == 200:
+        response_json = json.loads(response.content)
+        image = response_json['results'][0]['image'].get('url')
+        webbrowser.open(image)
 
 
 def main():
@@ -56,7 +86,7 @@ def main():
             elif cmd == 2:
                 succeeded = False
                 api_url = r'http://api.openweathermap.org/data/2.5/weather?q='
-                response = requests.get(api_url + params[1] + (r'&units=metric&APPID='+api_key))
+                response = requests.get(api_url + params[1] + (r'&units=metric&APPID='+weather_api_key))
                 if response.status_code == 200:
                     response_json = json.loads(response.content)
                     if response_json:
@@ -73,6 +103,10 @@ def main():
                     print('\x1B[3mSorry, I could not resolve the location you gave me.\x1B[0m')
             elif cmd == 3:
                 show_prompts()
+            elif cmd == 4:
+                show_stats(params[1].strip())
+            elif cmd == 5:
+                show_image(params[1].strip())
             elif cmd == 99:
                 # Similarity-based
                 print('\x1B[3mI did not get that, please try again.\x1B[0m')
