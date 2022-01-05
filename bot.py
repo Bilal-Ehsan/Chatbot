5d80c48b12cfbb57c58c074f92f99ea28c661f0b
Bilal-Ehsan <bilalehsan933@gmail.com>
2022-01-05 22:24:20 +0000

    Logical reasoning component refactored into a seperate file

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import pyttsx3
 from simpful import *
 
 import fuzzy
+import logic
 import similarity
 import web_services
 
@@ -15,45 +16,14 @@ import web_services
 load_dotenv()
 engine = pyttsx3.init()
 colorama.init(autoreset=True)
-read_expr = Expression.fromstring
 
 # The Kernel object is the public interface to the AIML interpreter
 kern = aiml.Kernel()
 kern.setTextEncoding(None)
 
-kb = []
-with open('knowledge_base.txt', 'r') as f:
-    for line in f:
-        stripped = line.strip()
-        if stripped:
-            kb.append(read_expr(stripped))
-
 with open('fuzzy_rules.txt') as f:
     rules = [rule.rstrip() for rule in f]
 fuzzy.FS.add_rules(rules)
-
-
-def integrity_check():
-    test_kb = kb.copy()
-    test_inputs = [
-        read_expr('-Evil(Superman)'),
-        read_expr('-Human(Groot)'),
-        read_expr('Avenger(Tony)'),
-        read_expr('Avenger(Steve)'),
-        read_expr('Avenger(Natasha)'),
-        read_expr('Avenger(Bruce)'),
-        read_expr('Avenger(Thor)'),
-        read_expr('Avenger(Clint)'),
-        read_expr('Enemies(Zod, Superman)')
-    ]
-
-    for i in range(len(test_inputs)):
-        result = ResolutionProver().prove(test_inputs[i], test_kb)
-        if result:
-            test_kb.append(test_inputs[i])
-        else:
-            print(Fore.LIGHTRED_EX + '\nInternal contradiction found! Exiting system...')
-            quit()
 
 
 def speak(text):
@@ -67,91 +37,6 @@ def show_prompts():
     print('Show me a picture of [superhero]')
     print('Show me a picture of a random superhero')
     print('Character threat calculator\n')
-
-
-def add_to_kb(q):
-    if 'and' in q:
-        object_1, object_2 = q.split(' and ')
-        expr = read_expr(f'Enemies({object_1.capitalize()}, {object_2.capitalize()})')
-    else:
-        object, subject = q.split(' is ')
-        expr = read_expr(f'{subject.capitalize()}({object.capitalize()})')
-
-    if expr in kb:
-        print(Fore.LIGHTMAGENTA_EX + 'I already knew that!')
-        speak('I already knew that!')
-        return
-
-    result = ResolutionProver().prove(expr, kb)
-    if result:
-        kb.append(expr)
-        print(Fore.LIGHTMAGENTA_EX + 'Okay, I\'ll remember that!')
-        speak('Okay, I\'ll remember that!')
-    else:
-        print(Fore.LIGHTMAGENTA_EX + 'Sorry, that contradicts with what I know!')
-        speak('Sorry, that contradicts with what I know!')
-
-
-def fact_check(fact):
-    if fact in kb:
-        print(Fore.LIGHTMAGENTA_EX + 'Incorrect')
-        speak('Incorrect')
-
-
-def is_in_kb(expr):
-    if expr not in kb:
-        print(Fore.LIGHTMAGENTA_EX + 'There\'s nothing about this in my knowledge base!')
-        speak('There\'s nothing about this in my knowledge base!')
-        return False
-    return True
-
-
-def is_multi_valued_in_kb(expr):
-    if expr not in kb:
-        return False
-    return True
-
-
-def check_kb(q):
-    if 'not' in q:
-        object, subject = q.split(' is not ')
-        expr = read_expr(f'-{subject.capitalize()}({object.capitalize()})')
-        if not is_in_kb(expr): return
-    elif 'and' in q:
-        object_1, object_2 = q.split(' and ')
-        expr = read_expr(f'Enemies({object_1.capitalize()}, {object_2.capitalize()})')
-        second_expr = read_expr(f'Enemies({object_2.capitalize()}, {object_1.capitalize()})')
-        in_kb = (not is_multi_valued_in_kb(expr)) and (not is_multi_valued_in_kb(second_expr))
-        if in_kb:
-            print(Fore.LIGHTMAGENTA_EX + 'There\'s nothing about this in my knowledge base!')
-            speak('There\'s nothing about this in my knowledge base!')
-            return
-    else:
-        object, subject = q.split(' is ')
-        expr = read_expr(f'{subject.capitalize()}({object.capitalize()})')
-        if not is_in_kb(expr): return
-
-    result = ResolutionProver().prove(expr, kb)
-    if result:
-        print(Fore.LIGHTMAGENTA_EX + 'That\'s correct!')
-        speak('That\'s correct!')
-    else:
-        print('I\'m not sure about that... Let me check...')
-
-        if 'is evil' in q:
-            fact_to_check = read_expr(f'Superhero({object.capitalize()})')
-            fact_check(fact_to_check)
-        elif 'is human' in q:
-            fact_to_check = read_expr(f'Alien({object.capitalize()})')
-            fact_check(fact_to_check)
-        elif subject == 'Avenger' or subject == 'avenger':
-            fact_to_check = read_expr(f'Marvel({object.capitalize()})')
-            if fact_to_check not in kb:
-                print(Fore.LIGHTMAGENTA_EX + 'Sorry, I don\'t know')
-                speak('Sorry, I don\'t know')
-        elif object == 'Kryptonite' or object == 'kryptonite':
-            print(Fore.LIGHTMAGENTA_EX + 'Incorrect')
-            speak('Incorrect')
 
 
 def character_threat_calculator():
@@ -182,7 +67,7 @@ def character_threat_calculator():
 
 
 def main():
-    integrity_check()
+    logic.integrity_check()
     kern.bootstrap(learnFiles='patterns.xml')
 
     print(Fore.LIGHTGREEN_EX + 'Welcome to the chatbot! I like to talk about superheroes')
@@ -224,9 +109,9 @@ def main():
             elif cmd == 8:
                 character_threat_calculator()
             elif cmd == 9:
-                add_to_kb(params[1].strip())
+                logic.add_to_kb(params[1].strip())
             elif cmd == 10:
-                check_kb(params[1].strip())
+                logic.check_kb(params[1].strip())
             elif cmd == 0:
                 similarity.similarity_check(params[1].strip())
         else:
---
+++ logic.py
@@ -0,0 +1,123 @@
+from colorama import Fore
+from nltk.sem import Expression
+from nltk.inference import ResolutionProver
+
+import bot
+
+
+read_expr = Expression.fromstring
+
+kb = []
+with open('knowledge_base.txt', 'r') as f:
+    for line in f:
+        stripped = line.strip()
+        if stripped:
+            kb.append(read_expr(stripped))
+
+
+def integrity_check():
+    test_kb = kb.copy()
+    test_inputs = [
+        read_expr('-Evil(Superman)'),
+        read_expr('-Human(Groot)'),
+        read_expr('Avenger(Tony)'),
+        read_expr('Avenger(Steve)'),
+        read_expr('Avenger(Natasha)'),
+        read_expr('Avenger(Bruce)'),
+        read_expr('Avenger(Thor)'),
+        read_expr('Avenger(Clint)'),
+        read_expr('Enemies(Zod, Superman)')
+    ]
+
+    for i in range(len(test_inputs)):
+        result = ResolutionProver().prove(test_inputs[i], test_kb)
+        if result:
+            test_kb.append(test_inputs[i])
+        else:
+            print(Fore.LIGHTRED_EX + '\nInternal contradiction found! Exiting system...')
+            quit()   
+
+
+def add_to_kb(q):
+    if 'and' in q:
+        object_1, object_2 = q.split(' and ')
+        expr = read_expr(f'Enemies({object_1.capitalize()}, {object_2.capitalize()})')
+    else:
+        object, subject = q.split(' is ')
+        expr = read_expr(f'{subject.capitalize()}({object.capitalize()})')
+
+    if expr in kb:
+        print(Fore.LIGHTMAGENTA_EX + 'I already knew that!')
+        bot.speak('I already knew that!')
+        return
+
+    result = ResolutionProver().prove(expr, kb)
+    if result:
+        kb.append(expr)
+        print(Fore.LIGHTMAGENTA_EX + 'Okay, I\'ll remember that!')
+        bot.speak('Okay, I\'ll remember that!')
+    else:
+        print(Fore.LIGHTMAGENTA_EX + 'Sorry, that contradicts with what I know!')
+        bot.speak('Sorry, that contradicts with what I know!')
+
+
+def fact_check(fact):
+    if fact in kb:
+        print(Fore.LIGHTMAGENTA_EX + 'Incorrect')
+        bot.speak('Incorrect')
+
+
+def is_in_kb(expr):
+    if expr not in kb:
+        print(Fore.LIGHTMAGENTA_EX + 'There\'s nothing about this in my knowledge base!')
+        bot.speak('There\'s nothing about this in my knowledge base!')
+        return False
+    return True
+
+
+def is_multi_valued_in_kb(expr):
+    if expr not in kb:
+        return False
+    return True
+
+
+def check_kb(q):
+    if 'not' in q:
+        object, subject = q.split(' is not ')
+        expr = read_expr(f'-{subject.capitalize()}({object.capitalize()})')
+        if not is_in_kb(expr): return
+    elif 'and' in q:
+        object_1, object_2 = q.split(' and ')
+        expr = read_expr(f'Enemies({object_1.capitalize()}, {object_2.capitalize()})')
+        second_expr = read_expr(f'Enemies({object_2.capitalize()}, {object_1.capitalize()})')
+        in_kb = (not is_multi_valued_in_kb(expr)) and (not is_multi_valued_in_kb(second_expr))
+        if in_kb:
+            print(Fore.LIGHTMAGENTA_EX + 'There\'s nothing about this in my knowledge base!')
+            bot.speak('There\'s nothing about this in my knowledge base!')
+            return
+    else:
+        object, subject = q.split(' is ')
+        expr = read_expr(f'{subject.capitalize()}({object.capitalize()})')
+        if not is_in_kb(expr): return
+
+    result = ResolutionProver().prove(expr, kb)
+    if result:
+        print(Fore.LIGHTMAGENTA_EX + 'That\'s correct!')
+        bot.speak('That\'s correct!')
+    else:
+        print('I\'m not sure about that... Let me check...')
+
+        if 'is evil' in q:
+            fact_to_check = read_expr(f'Superhero({object.capitalize()})')
+            fact_check(fact_to_check)
+        elif 'is human' in q:
+            fact_to_check = read_expr(f'Alien({object.capitalize()})')
+            fact_check(fact_to_check)
+        elif subject == 'Avenger' or subject == 'avenger':
+            fact_to_check = read_expr(f'Marvel({object.capitalize()})')
+            if fact_to_check not in kb:
+                print(Fore.LIGHTMAGENTA_EX + 'Sorry, I don\'t know')
+                bot.speak('Sorry, I don\'t know')
+        elif object == 'Kryptonite' or object == 'kryptonite':
+            print(Fore.LIGHTMAGENTA_EX + 'Incorrect')
+            bot.speak('Incorrect')
